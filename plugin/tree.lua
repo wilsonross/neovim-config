67bc74a45d4f845d791f11b60a5e3513bc1eb8fa
Ross Wilson <ross.wilson@findross.dev>
2022-12-17 13:12:38 +0000

    Removed clear clipboard commands
    * The clear_clipboard function was attempting
      to call utils.notify when it should have been
      notify

--- plugin/tree.lua
+++ plugin/tree.lua
@@ -28,15 +28,4 @@ local api = require("nvim-tree.api")
 
 -- Mappings
 vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", {noremap = true})
-vim.keymap.set("n", "<leader>tf", "<cmd>NvimTreeFocus<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>tc", "<cmd>NvimTreeCollapse<CR>", {noremap = true})
-
-vim.keymap.set("n", "<leader>c", function ()
-	api.clear_clipboard()
-	api.fs.copy()
-end, {noremap = true})
-
-vim.keymap.set("n", "<leader>x", function ()
-	api.clear_clipboard()
-	api.fs.cut()
-end, {noremap = true})
